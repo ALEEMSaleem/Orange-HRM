93f6d69c8fd00defce6b640d8fad481d52057898
muhammad saleem <sarowerhome@gmail.com>
2024-03-06 23:13:40 -0500

    Update addEmploye

--- cypress/e2e/addemployee.cy.js
+++ cypress/e2e/addemployee.cy.js
@@ -1,13 +1,20 @@
 
 
 describe("Add Employee", () => {
-  it("addEmployee", () => {
+  it("addEmployeeValidation", () => {
     cy.login();
     cy.get(":nth-child(2) > .oxd-main-menu-item > .oxd-text").click();
     cy.get(".oxd-topbar-body-nav > ul :nth-child(3)").click();
-    cy.get("input[name='firstName']").type('Mian');
-    cy.get("input[name='lastName']").type('Aleem');
+    cy.get("input[name='firstName']").type('steve');
+    cy.get("input[name='lastName']").type('young');
+    cy.get('div:nth-child(2) > div:nth-child(1) > div > div:nth-child(2) > input').type('112233');
     cy.get("button[type='submit']").click();
+    cy.contains("Successfully Saved").should("be.visible");
+
+    cy.get('div:nth-child(2) > div > div:nth-child(2) > div > div > input').click();
+    
+   
+
    
   });
 
